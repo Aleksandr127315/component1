bee1b3bf695dc77eab274930000c30e22c45f6c0
Aleksandr Merzlikin <merzlikina730@gmail.com>
2025-09-04 21:20:34 +0300

    add comrnts and date

--- src/App.jsx
+++ src/App.jsx
@@ -4,49 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
 
-// export function App() {
-//   const [count, setCount] = useState(0)
-//   const date = new Date().getFullYear()
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//       <footer>
-//         {date} год
-//       </footer>
-//     </>
-//   )
-// }
+
 export function App(){
     const [count,setCount] = useState(0);
-    const date = new Date().getFullYear()
-      return React.createElement(
+    const date = new Date().getFullYear() //декларативный стиль
+      return React.createElement(// можно сказать что вся функция это декларативный стиль
       "div",
       null,
-      React.createElement(
+      React.createElement( //декларативный
         "a",
         { href: "https://vite.dev", target: "_blank" },
         React.createElement("img", { src: viteLogo, className: "logo", alt: "Vite logo" })
       ),
-      React.createElement(
+      React.createElement(//декларативный
         "a",
         { href: "https://react.dev", target: "_blank" },
         React.createElement("img", { src: reactLogo, className: "logo react", alt: "React logo" })
@@ -55,25 +25,25 @@ export function App(){
       React.createElement(
         "div",
         { className: "card" },
-        React.createElement(
+        React.createElement(//предполагаю что императивный,потому что мы сами прописываем логику
           "button",
           { onClick: () => setCount((count) => count + 1) },
           `count is ${count}`
         ),
-        React.createElement(
+        React.createElement(//декларативный
           "p",
           null,
           "Edit ",
-          React.createElement("code", null, "src/App.jsx"),
+          React.createElement("code", null, "src/App.jsx"),//декларативный
           " and save to test HMR"
         )
       ),
-      React.createElement(
+      React.createElement(//декларативный
         "p",
         { className: "read-the-docs" },
         "Click on the Vite and React logos to learn more"
       ),
-      React.createElement("footer", null, `${date} год.`)
+      React.createElement("footer", null, `${date} год.`)//декларативный
     );
 
 }
